e27e1a3bae323b5b471b2c1ca0e543881c1fad80
Harish Arvind SELVAKUMAR <98465967+Harish-Arvind@users.noreply.github.com>
2025-05-13 10:55:10 +0200

    Delete Board_Games_Advanced_Database-main directory

--- Board_Games_Advanced_Database-main/Board-Games/Board-Games-App/App-frontend/src/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const API_BASE = "http://localhost:5000"; // ✅ Corrigé
-
-export async function getGames() {
-  const response = await axios.get(`${API_BASE}/api/games`);
-  return response.data;
-}
